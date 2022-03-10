58b9e5fdcf7bb7b30bcd00cbc0f471f5b7c2ead7
kingnan-Guo <373734199@qq.com>
2022-03-10 22:52:47 +0800

    创建指针 链表

---
+++ code/linkedList/linkedList.c
@@ -0,0 +1,11 @@
+/**
+ * @file linkedList.c
+ * @author your name (you@domain.com)
+ * @brief 链表
+ * @version 0.1
+ * @date 2022-03-10
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include "stdio.h"
---
+++ code/pointer/pointer.c
@@ -0,0 +1,23 @@
+/**
+ * @file pointer.c
+ * @author your name (you@domain.com)
+ * @brief 指针
+ * @version 0.1
+ * @date 2022-03-10
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include "stdio.h"
+
+
+
+int main()
+{
+    /* code */
+    return 0;
+}
+
+
+
+ 
