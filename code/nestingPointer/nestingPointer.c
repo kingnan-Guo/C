645c60349303069685132fda50970c09238c0592
kingnan-Guo <373734199@qq.com>
2022-03-18 00:56:01 +0800

    更新 对 指针 **p 的理解， 指针作为传参数 的 *的个数

--- code/nestingPointer/nestingPointer.c
+++ code/nestingPointer/nestingPointer.c
@@ -16,7 +16,7 @@
 
 
 // 
-void test(int **p){
+void test(int * * p){
     printf(" ============ \n");
     printf("**p = %d \n", **p);
     printf("&(**p) = %d \n", &(**p));
@@ -31,7 +31,10 @@ void test(int **p){
 
 
 
-// void test(int ***p) 的传参数 带几个 * 都没关系，因为重要的是 传过来的 P1； P1 为 test(&p) 中 的 &p；也就是 p1 = p的地址
+// void test(int ***p) 的传参数 带几个 * 都没关系，不过要大于指针的 “内部使用的” 的个数，
+// 比如这段程序如果 内部调用 [printf("**p = %d \n", **p1)] 中  * 的个数大于 1， 那么不允许传入 1 个，
+// 如果只是用一个 printf("*p = %d \n", *p); 那么可以传 一个以上， p1 依然等于 p的地址
+// 因为重要的是 传过来的 P1； P1 为 test(&p) 中 的 &p；也就是 p1 = p的地址
 // void test(int ***p1){
 //     printf(" ============ \n");
 //     printf("**p = %d \n", **p1);
