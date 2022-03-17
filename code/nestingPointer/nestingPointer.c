28ebaba59c94032c6df6f547b2b3752ef5d76d2e
kingnan-Guo <373734199@qq.com>
2022-03-18 00:47:23 +0800

    更新对指针的 理解

--- code/nestingPointer/nestingPointer.c
+++ code/nestingPointer/nestingPointer.c
@@ -13,6 +13,9 @@
 
 #include "stdio.h"
 
+
+
+// 
 void test(int **p){
     printf(" ============ \n");
     printf("**p = %d \n", **p);
@@ -20,10 +23,25 @@ void test(int **p){
     printf("*p = %d \n", *p);
     printf("p = %d \n", p);
     printf("&p = %d \n", &p);
+
+    // printf("***p = %d \n", ***p);
     
 }
 
 
+
+
+// void test(int ***p) 的传参数 带几个 * 都没关系，因为重要的是 传过来的 P1； P1 为 test(&p) 中 的 &p；也就是 p1 = p的地址
+// void test(int ***p1){
+//     printf(" ============ \n");
+//     printf("**p = %d \n", **p1);
+//     printf("&(**p) = %d \n", &(**p1));
+//     printf("*p = %d \n", *p1);
+//     printf("p = %d \n", p1);
+//     printf("&p = %d \n", &p1);
+
+//     // printf("***p = %d \n", ***p1);
+// }
 
 
 
---
+++ code/pointer/pointSecond.c
@@ -0,0 +1,17 @@
+/**
+ * @file pointSecond.c
+ * @author your name (you@domain.com)
+ * @brief 指针变量作为函数参数
+ * @version 0.1
+ * @date 2022-03-18
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
