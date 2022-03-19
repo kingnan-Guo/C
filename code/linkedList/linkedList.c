4da0b1667b3752a4dfd4442d720859327f73c435
kingnan-Guo <373734199@qq.com>
2022-03-19 22:35:53 +0800

    对 链表 有新的 认识 和新的 标注

--- code/linkedList/linkedList.c
+++ code/linkedList/linkedList.c
@@ -64,30 +64,36 @@ int main(void)
     /* 最初的内存分配 */
     // C 库函数 void *malloc(size_t size) 分配所需的内存空间，并返回一个指向它的指针。
     // (struct Node *) 将返回的指针至为 Node 的类型
-    // *header 为当前 通过malloc  创建出来的内存空间返回的指针
+    // header 为当前 通过malloc  创建出来的内存空间返回的指针
     // header 为当前 新创建内存空间的 值
 
     struct Node *  header = (struct Node *) malloc(sizeof(struct Node));
     // strcpy(str->value, 2);
     // printf("String = %s,  Address = %u \n", header, header);
 
-    //将header （值） 作为 value 传给 *flag ，那么flag->next存储的就是 h
-    struct Node *  flag = header; 
+    //将 header （ 地址） 作为 value 传给 flag ，那么flag 作为起始点的 标记为
+    struct Node *  flag = header;  // 等价于 struct Node *  flag; flag = header;
     // printf("flag == %x \n", flag);
+    // 先输入 标记为  的 value
     scanf("%d", &flag -> value);
     // printf("flag == %x \n", flag->value);
     while (flag->value != 0)
     {
+        // 如果标记位 不为 0  ，那么开辟新的  储存空间。并把新开辟的 储存空间 的地址付给flag 标记位
         flag-> next =  (struct Node *) malloc(sizeof(struct Node));
         // printf("String = %s,  Address = %u\n", flag, flag);
+        // 将 flag 标记位 移动到下一个节点，就是新开辟的 空间
         flag = flag->next;
         scanf("%d", &flag -> value);
     }
     // printf("flag == %x \n", flag->value); 
+    // 将最终的节点 的 next 指针位 赋值 为 空 作为 链尾
     flag->next = NULL;
+    // 将flag 标识位 移动到header 头部
     flag=header;
     // printf('%s============');
     printf(" \n");
+    // 开始循环输出标志位
     while (flag != NULL)
     {
         printf("flag->value == %d \n", flag->value);
